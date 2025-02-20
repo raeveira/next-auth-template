b554959f5fd3b2d2fd00dabe5843c287258c18ef
raeveira <rae@raeveira.nl>
2025-02-20 10:50:32 +0100

    added github provider linking and logout button

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 import {useSession} from "next-auth/react";
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import {linkGithub} from "@/app/actions/auth/linkProviders";
+import {useRouter} from "next/navigation";
 
 
 const Dashboard = () => {
     const {data: session, status} = useSession();
+    const router = useRouter();
 
     return (
         <div>
@@ -16,6 +20,8 @@ const Dashboard = () => {
                     <p>Welcome, {session.user.email}</p>
                     <Image src={session.user.image || '/default'} alt={session.user.name} width={100} height={100}
                     />
+                    <Button onClick={() => linkGithub()}>Link Github</Button>
+                    <Button onClick={() => router.push('/logout')}>Sign Out</Button>
                 </>
             )}
         </div>
