f8a864801dd7ee10dea6b4d5f24afaf100087c3d
raeveira <rae@raeveira.nl>
2025-02-20 09:25:18 +0100

    updated with session

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+import {useSession} from "next-auth/react";
+import Image from "next/image";
+
+
 const Dashboard = () => {
+    const {data: session, status} = useSession();
+
     return (
         <div>
-        <h1>Home</h1>
+            {status === "loading" && <p>Loading...</p>}
+            {status === "unauthenticated" && <p>Access Denied</p>}
+            {status === "authenticated" && (
+                <>
+                    <p>Access Granted</p>
+                    <p>Welcome, {session.user.email}</p>
+                    <Image src={session.user.image || '/default'} alt={session.user.name} width={100} height={100}
+                    />
+                </>
+            )}
         </div>
     );
 }
